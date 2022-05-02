49959dcebae7303737b747b268bdb9227355c0c1
beauregardhenry <beauregard.henry@gmail.com>
2022-05-02 04:03:03 -0500

    Initial commit

---
+++ test/technical-challenge.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechnicalChallenge from '../lib/technical-challenge-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/technical-challenge-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechnicalChallenge.TechnicalChallengeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
